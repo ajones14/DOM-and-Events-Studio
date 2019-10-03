fba8dd59e925952201bc1654b249c0056c75911e
Abigail Wiggins <rebtaylor14@gmail.com>
2019-10-03 12:25:00 -0500

    Landing button finished

--- scripts.js
+++ scripts.js
@@ -2,17 +2,26 @@
 // Remember to pay attention to page loading!
 
 function init () {
+  let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");  
+  let shuttleBackground = document.getElementById("shuttleBackground");  
+  let flightStatus = document.getElementById("flightStatus");
+
   let takeoffButton = document.getElementById("takeoff");
   takeoffButton.addEventListener("click", function () {
     let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
     if (response === true) {
-      let p = document.getElementById("flightStatus");
-      p.innerHTML = "Shuttle in flight.";
-      let shuttleBackground = document.getElementById("shuttleBackground");
+      flightStatus.innerHTML = "Shuttle in flight.";
       shuttleBackground.style.backgroundColor = "blue";
-      let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
       spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
     }
+  });
+
+  let landingButton = document.getElementById("landing");
+  landingButton.addEventListener("click", function() {
+      window.alert("The shuttle is landing. Landing gear engaged.");
+      flightStatus.innerHTML = "The shuttle has landed.";
+      shuttleBackground.style.backgroundColor = "green";
+      spaceShuttleHeight.innerHTML = 0;
   });
 }
 

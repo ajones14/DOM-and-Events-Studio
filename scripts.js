c74db2cf6ca2718ef105e528061bb07d1a8316bd
Abigail Wiggins <rebtaylor14@gmail.com>
2019-10-03 12:13:01 -0500

    Take off button completed

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init () {
+  let takeoffButton = document.getElementById("takeoff");
+  takeoffButton.addEventListener("click", function () {
+    let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+    if (response === true) {
+      let p = document.getElementById("flightStatus");
+      p.innerHTML = "Shuttle in flight.";
+      let shuttleBackground = document.getElementById("shuttleBackground");
+      shuttleBackground.style.backgroundColor = "blue";
+      let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+      spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+    }
+  });
+}
+
+window.onload = init;
